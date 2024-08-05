194a279d6e231d4a389e85b8c13a4952b66e2680
yqt5420 <54099324+yqt5420@users.noreply.github.com>
2024-08-05 19:40:50 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -32,7 +32,17 @@ change_mirror() {
     sed -i 's/#Color/Color/g' /etc/pacman.conf
     echo '换源操作结束'
 }
+# 检查分区是否存在
+check_partition() {
+    local partition="$1"
 
+    # 使用ls -l命令检查分区是否存在，$devdir是设备目录，通常是/dev
+    local devdir=$(dirname "$partition")
+    if [ ! -d "$devdir" ] || [ ! -e "$partition" ]; then
+        echo "错误：分区设备 $partition 不存在。"
+        exit 1
+    fi
+}
 
 # 确保脚本以root权限运行
 if [[ $EUID -ne 0 ]]; then
@@ -52,6 +62,10 @@ if [[ -z "$boot_dir" || -z "$root_dir" ]]; then
     echo "错误：分区路径不能为空。"
     exit 1
 fi
+# 检查用户输入的分区是否存在
+check_partition "$boot_dir"
+check_partition "$root_dir"
+
 #格式化boot分区
 mkfs.fat -F32 "$boot_dir"
 #格式化根分区
@@ -62,9 +76,9 @@ btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home 
 umount /mnt
 # 挂载根分区
-mount -o subvol=@,compress=lzo "$root_dir" /mnt
+mount -t btrfs -o subvol=/@,compress=lzo "$root_dir" /mnt
 # 挂载 /home 子卷
-mount -o subvol=@home,compress=lzo "$root_dir" /mnt/home
+mount -t btrfs -o subvol=/@home,compress=lzo "$root_dir" /mnt/home
 # 挂载 boot 分区
 mount "$boot_dir" /mnt/boot
 
@@ -73,29 +87,40 @@ mount "$boot_dir" /mnt/boot
 
 ## -------------------------------------------------------------- ##
 # 给新系统安装基础软件
-pacstrap /mnt base base-devel linux linux-firmware btrfs-progs
-# 安装微码
-pacstrap /mnt pacman -S intel-ucode # Intel
-pacstrap /mnt pacman -S amd-ucode # AMD
+pacstrap /mnt base base-devel linux linux-firmware btrfs-progs intel-ucode amd-ucode
 
 # 安装常用软件
-pacstrap /mnt networkmanager vim sudo fish git wget nano htop neofetch yay
 
-#安装引导程序
-pacstrap /mnt grub efibootmgr os-prober
+# 安装中文字体
 ## -------------------------------------------------------------- ##
-
 # 生成 fstab 文件
 genfstab -U /mnt > /mnt/etc/fstab
 
 
 
 # 复制配置文件到新系统
+cp -r /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+cp -r /etc/pacman.conf /mnt/etc/pacman.conf
+
+echo '#############################################################################################################'
+echo '安装完成，现在进入arch-chroot开始配置系统'
+echo '###############################################################################################################\n'
 # 进入 Arch Linux 根文件系统环境
 arch-chroot /mnt /bin/bash << 'EOF'
 # 在这里执行一些命令，比如安装软件包
-#换源
-change_mirror
+pacman -Sy --need --noconfirm networkmanager vim sudo fish git wget nano htop neofetch
+pacman -Sy --need --noconfirm yay 
+pacman -Sy --need --noconfirm openssh 
+pacman -Sy --need --noconfirm screen  
+pacman -Sy --need --noconfirm wireless-regdb wireless_tools wpa_supplicant 
+pacman -Sy --need --noconfirm cronie 
+pacman -Sy --need --noconfirm wqy-zenhei 
+pacman -Sy --need --noconfirm timeshift 
+# 引导程序
+pacman -Sy --need --noconfirm grub efibootmgr os-prober
+
+
+
 #设置系统语言为中文
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
@@ -114,22 +139,33 @@ hwclock --systohc
 
 # 设置 root 密码
 echo "root:aw" | chpasswd
+usermod -s /bin/fish root 
 # 创建用户
-useradd -m -G wheel -s /bin/bash aw
+useradd -m -G wheel -s /bin/fish aw
 echo "aw:aw" | chpasswd
 # 给用户添加 sudo 权限
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+# 创建用户目录
+mkdir -p /home/aw
+chown -R aw:aw /home/aw
 
 # 安装引导程序
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH
 #修改grub配置文件
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 modprobe.blacklist=iTCO_wdt"/g' /etc/default/grub
+# modprobe.blacklist=iTCO_wdt
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 nowatchdog"/g' /etc/default/grub
 sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
 #生成grub配置文件
 grub-mkconfig -o /boot/grub/grub.cfg
 
-#在这里调用刚才封装的换源函数，给新系统换源
+echo 'Arch基础系统安装完成！'
+# 设置开机自启的任务
+# 设置网络开机自启
+systemctl enable NetworkManager
+systemctl enable sshd
+systemctl enable cronie
 EOF
 # 使用 exit 命令退出 arch-chroot 环境
 exit
+
 
